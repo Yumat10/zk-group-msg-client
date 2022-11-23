ee02fd5cbb15e34c3494bd98c547fdf835a7e845
yuma <mr.yuma.tanaka@gmail.com>
2022-11-23 16:01:48 -0500

    .page and .api file extensions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  pageExtensions: ['page.tsx', 'api.tsx', 'api.ts', '.page.js'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
